cead8c0ce382f9aa24dd4286037f2fca84ca6fd7
yukimatJP <yukimat.jp@gmail.com>
2024-09-28 09:01:59 +0900

    update sensors

--- src/main.js
+++ src/main.js
@@ -6,33 +6,33 @@ const TARGET_SENSOR_IDS = [
   {installed: true,  name: "656広場"}, // 1
   {installed: true,  name: "FabLab Saga"}, // 2
   {installed: true,  name: "たなかさんち"}, // 3
-  {installed: true,  name: "エスプラッツ（南"}, // 4
-  {installed: true,  name: "エスプラッツ（南東"}, // 5
-  {installed: true,  name: "エスプラッツ（西"}, // 6
-  {installed: true,  name: "エスプラッツ（北"}, // 7
+  {installed: true,  name: "エスプラッツ（南）"}, // 4
+  {installed: true,  name: "エスプラッツ（南東）"}, // 5
+  {installed: true,  name: "エスプラッツ（西）"}, // 6
+  {installed: true,  name: "エスプラッツ（北）"}, // 7
   {installed: true,  name: "トネリコ・カフェ"}, // 8
   {installed: true,  name: "白山名店街（自動販売機横）"}, // 9
   {installed: true,  name: "スマート街なかテラス"}, // 10,
   {installed: true,  name: "鮨・椿油天ぷら八木"}, // 11
-  {installed: true,  name: "緑道広場（サイネージ"}, // 12
-  {installed: true,  name: "SUNRISE POCKET（サイネージ"}, // 13
-  {installed: true,  name: "白山テラス（サイネージ"}, // 14
+  {installed: true,  name: "緑道広場（サイネージ）"}, // 12
+  {installed: true,  name: "SUNRISE POCKET（サイネージ）"}, // 13
+  {installed: true,  name: "白山テラス（サイネージ）"}, // 14
   {installed: true,  name: "E.A.D"}, // 15
   {installed: true,  name: "村岡屋 本店"}, // 16
-  {installed: false, name: ""}, // 17
+  {installed: false,  name: "駅前中央通り②（とも蔵・わん前）"}, // 17
   {installed: true,  name: "駅前交流広場"}, // 18
   {installed: true,  name: "えきまち一丁目"}, // 19
   {installed: true,  name: "サガハツ"}, // 20,
-  {installed: true,  name: "佐賀駅バスセンター（西側"}, // 21
-  {installed: true,  name: "佐賀駅バスセンター（構内"}, // 22
-  {installed: false, name: ""}, // 23
+  {installed: true,  name: "佐賀駅バスセンター（西側）"}, // 21
+  {installed: true,  name: "佐賀駅バスセンター（構内）"}, // 22
+  {installed: false,  name: "駅前中央通り①（ニッポンレンタカー前）"}, // 23
   {installed: true,  name: "かど屋佐賀駅北口店"}, // 24
   {installed: true,  name: "ROUGH cafe&dining bar"}, // 25
-  {installed: true,  name: "まちかど広場（サイネージ"}, // 26
+  {installed: true,  name: "まちかど広場（サイネージ）"}, // 26
   {installed: true,  name: "もしもしかめさん"}, // 27
   {installed: true,  name: "ANZee."}, // 28
-  {installed: true,  name: "SAGAアリーナ"}, // 29
-  {installed: false, name: ""}, // 30
+  {installed: false,  name: "SAGAアリーナ"}, // 29
+  {installed: false,  name: "駅前中央通り③（三井住友銀行前）"}, // 30
   {installed: false, name: ""}, // 31
   {installed: false, name: ""}, // 32
   {installed: false, name: ""}, // 33
@@ -120,7 +120,7 @@ var app = new Vue({
       });  
     },
     setCurrentTime: function(offset) {
-      this.option.start_at = parseInt(Util.getCurrentTime()/1000 - 100 * 10); // 10 min
+      this.option.start_at = parseInt(Util.getCurrentTime()/1000 - 60 * 60); // 60 min
       this.option.end_at   = parseInt(Util.getCurrentTime()/1000      );
     },
     changeMode(mode) {
@@ -137,6 +137,7 @@ var app = new Vue({
     isDashboardMode: function() { return this.mode == 'dashboard'; },
     isSummaryMode: function() { return this.mode == 'summary'; },
     isDetailedMode: function() { return this.mode == 'detailed'; },
+    targetPeriod: function() { return Util.getDatetimeStr(this.option.start_at*1000) + " 〜 " + Util.getDatetimeStr(this.option.end_at*1000); },
   }
 });
 
